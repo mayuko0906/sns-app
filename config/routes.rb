d806c672c4cb8933a0669b56e85d572778ad5d8c
mayuko0906 <mayukoiino0906@gmail.com>
2020-05-31 22:45:20 +0900

    Update routes.rb

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 Rails.application.routes.draw do
   devise_for :users
-  root to: 'posts#timeline'
+  root to: 'posts#index'
   resources :users, only: [:index, :show, :edit , :update] do
     resource :follows, only: [:create, :destroy]
     get :favorites, on: :member
