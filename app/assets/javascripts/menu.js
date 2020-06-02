5d1864895333cff2662eaca2b97e96b746c628f7
mayuko0906 <mayukoiino0906@gmail.com>
2020-06-02 11:11:01 +0900

    chaneges

--- app/assets/javascripts/menu.js
+++ app/assets/javascripts/menu.js
@@ -1,14 +1,14 @@
 $(function (){
 
   $('.dropdown-btn').hover(
-  function() {
-    //カーソルが重なった時
-    $(this).children('.dropdown').addClass('open');
-  }, function() {
-    //カーソルが離れた時
-    $(this).children('.dropdown').removeClass('open');
-  }
-);
+    function() {
+      //カーソルが重なった時
+      $(this).children('.dropdown').addClass('open');
+    }, function() {
+      //カーソルが離れた時
+      $(this).children('.dropdown').removeClass('open');
+    }
+  );
 // $( document ).ready(function() {
 //   // メッセージを３秒後にフェードアウトする
 // });
@@ -25,4 +25,42 @@ $(function (){
         $('.notification').slideUp('slow');
     },1500);
 
-});
+    // // $('.drawer').drawer();
+    // $('.drawer').drawer();
+
+
+    // $(".drawer-toggle").on("click", function(){
+    //   // ハンバーガーメニューの位置を設定
+    //   // var rightVal = 0;
+    //   if($('#hamburger-navi').hasClass("open")) {
+    //       // 位置を移動させメニューを開いた状態にする
+    //       rightVal = -300;
+    //       // メニューを開いたら次回クリック時は閉じた状態になるよう設定
+    //       $('#hamburger-navi').removeClass("open");
+    //   } else {
+    //       // メニューを開いたら次回クリック時は閉じた状態になるよう設定
+    //       $('#hamburger-navi').addClass("open");
+    //   }
+
+   
+    $('.nav-toggle').on('click', function() {
+        // $('.gloval-nav').addClass('open');
+        // $('.gloval-nav').removeClass('open');
+
+        if($('.gloval-nav').hasClass("open")) {
+          // 位置を移動させメニューを開いた状態にする
+          rightVal = -300;
+          // メニューを開いたら次回クリック時は閉じた状態になるよう設定
+          $('.gloval-nav').removeClass("open");
+      } else {
+          // メニューを開いたら次回クリック時は閉じた状態になるよう設定
+          $('.gloval-nav').addClass("open");
+      }
+
+
+
+    });
+
+  });
+
+
