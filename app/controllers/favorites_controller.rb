628cdfd4238951105e0919852fb54c79803222c8
mayuko0906 <mayukoiino0906@gmail.com>
2020-06-30 15:52:38 +0900

    いいね機能の非同期通信化

--- app/controllers/favorites_controller.rb
+++ app/controllers/favorites_controller.rb
@@ -2,20 +2,21 @@ class FavoritesController < ApplicationController
   # before_filter :require_login
  
   def create
-    @post    = Post.find(params[:post_id])
+    @post = Post.find(params[:post_id])
     @favorite = current_user.favorites.build(post: @post)
-
-    if @favorite.save
-      redirect_back(fallback_location: root_path ,notice: "お気に入りに登録しました")
-    else
-      redirect_to posts_url , alert: "このツイートはお気に入りに登録できません"
-   end
+    @favorite.save
+    # if @favorite.save
+    #   redirect_back(fallback_location: root_path ,notice: "お気に入りに登録しました")
+    # else
+    #   redirect_to posts_url , alert: "このツイートはお気に入りに登録できません"
+  #  end
   end
 
   def destroy
+    @post = Post.find(params[:post_id])
     @favorite = current_user.favorites.find_by!(post_id: params[:post_id])
     @favorite.destroy
-    redirect_back(fallback_location: root_path ,notice: "お気に入りを解除しました")
+    # redirect_back(fallback_location: root_path ,notice: "お気に入りを解除しました")
   end
 
 end
