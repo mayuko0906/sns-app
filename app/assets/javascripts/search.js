91f2e5b7aefd68d8ee62d55824a7b2141d1ee7d4
mayuko0906 <mayukoiino0906@gmail.com>
2020-07-05 23:55:26 +0900

    検索機能

--- app/assets/javascripts/search.js
+++ app/assets/javascripts/search.js
@@ -26,29 +26,31 @@ $(function() {
   function appendPost(post) {
     var html = `<div class="post">
                   <div class="user-icon">
-                    <a href="/users/11"><img src="/uploads/user/image/11/hns0012-013.jpg"></a>
+                    <a href="/users/${post.user_id}">
+                      <img src="#">
+                    </a>
                   </div>
                   <div class="content">
                     <div class="content-top">
                       <div class="user-name">
-                        <a href="/users/11">ハチワレ</a>
+                        <a href="/users/${post.user_id}">${post.name}</a>
                       </div>
-                    <div class="post-content">lkk
+                    <div class="post-content">${post.post}
                       <div class="post-content__image">
-                        <a data-lightbox="#<Image:0x00007f9fe955f150>" rel="lightbox" href="/uploads/image/image_url/102/was0015-049.jpg"><img class="popup-image" src="/uploads/image/image_url/102/was0015-049.jpg"></a>
+                        <a data-lightbox="#<Image:0x00007f9fe955f150>" rel="lightbox" href="#"><img class="popup-image" src="#"></a>
                       </div>
                     </div>
-                    <div class="post-time">2020-07-03 13:43</div>
+                    <div class="post-time"> ${post.created_at}</div>
                   </div>
                     <div class="content-bottom">
-                      <div class="favorite-wrapper" id="favorites_buttons_126">
+                      <div class="favorite-wrapper" id="favorites_buttons_${post.id}">
                         <div class="favorite-btn">
-                          <a data-remote="true" rel="nofollow" data-method="post" href="/posts/126/favorites"><i class="fa fa-heart fav-add"></i></a>
+                          <a data-remote="true" rel="nofollow" data-method="post" href="/posts/${post.id}/favorites"><i class="fa fa-heart fav-add"></i></a>
                         </div>
                       </div>
                       <div class="destoroy-follow-wrapper">
                         <div class="destroy-btn">
-                        <a rel="nofollow" data-method="delete" href="/posts/126">削除</a>
+                        <a rel="nofollow" data-method="delete" href="/posts/${post.id}">削除</a>
                         </div>
                       </div>
                     </div>
@@ -56,6 +58,43 @@ $(function() {
                 </div>`
     search_list.append(html);
   }
+
+
+
+<div class="post">
+  <div class="user-icon">
+    <a href="/users/10">
+      <div class="no-image">
+        no image
+      </div>
+    </a>
+  </div>
+  <div class="content">
+    <div class="content-top">
+      <div class="user-name">
+        <a href="/users/10">みーこ</a>
+      </div>
+      <div class="post-content">
+        こんばんは
+        <div class="post-content__image"></div>
+      </div>
+      <div class="post-time">2020-06-01 17:25</div>
+    </div>
+    <div class="content-bottom">
+      <div class="favorite-wrapper" id="favorites_buttons_85">
+        <div class="favorite-btn">
+          <a data-remote="true" rel="nofollow" data-method="post" href="/posts/85/favorites"><i class="fa fa-heart fav-add"></i></a>
+          <div class="favorite-count">1</div>
+        </div>
+      </div>
+      <div class="destoroy-follow-wrapper">
+        <div class="follow-btn follows_buttons_10">
+          <a data-remote="true" rel="nofollow" data-method="post" href="/users/10/follows">フォローする</a>
+        </div>
+      </div>
+    </div>
+  </div>
+</div>
 
 
   $(".search-input-post").on("keyup", function() {
@@ -84,22 +123,22 @@ $(function() {
   $(".search-input-user").on("keyup", function() {
     var input = $(".search-input-user").val();
     console.log(input);
-    // $.ajax({
-    //   type: 'GET',
-    //   url: '/posts/search',
-    //   data: { keyword: input },
-    //   dataType: 'json'
-    // })
-    // .done(function(posts) {
-    //   search_list.empty();
-    //   if (posts.length !== 0) {
-    //     posts.forEach(function(post){
-    //       appendPost(post;
-    //     });
-    //   }
-    //   else {
-    //     appendErrMsgToHTML("一致する投稿がありません");
-    //   }
-    // })
+    $.ajax({
+      type: 'GET',
+      url: '/posts/search',
+      data: { keyword: input },
+      dataType: 'json'
+    })
+    .done(function(posts) {
+      search_list.empty();
+      if (posts.length !== 0) {
+        posts.forEach(function(post){
+          appendPost(post);
+        });
+      }
+      else {
+        appendErrMsgToHTML("一致する投稿がありません");
+      }
+    })
   });
 });
--- app/views/posts/search.json.jbuilder
+++ app/views/posts/search.json.jbuilder
@@ -1,7 +1,9 @@
 json.array! @posts do |post|
   json.id post.id
   json.post post.post
+  json.created_at post.created_at
   json.images post.images
   json.user_id post.user_id
+  json.name post.user.name
   json.user_sign_in current_user
 end
